9aee45cfa81e1ef0b014cfcbd14f4b8922d2a467
heeyio <kevinfritsche@gmail.com>
2023-03-19 14:22:04 +0100

    added fist sample schema

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+// schemas/pet.js
+export default {
+  name: 'pet',
+  type: 'document',
+  title: 'Pet',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+  ],
+}
